9a99fc8e7df34bf374044a95bec9fbd7fac42016
Bhupen <bhupensahoo200127@gmail.com>
2023-10-17 07:40:14 +0530

    header in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,31 +3,13 @@ import Summery from "../components/Summery";
 import BarChart from "../components/BarChart";
 import PieChart from "../components/PieChart";
 import TableData from "../components/TableData";
-import { BsSearch } from "react-icons/bs";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
     <div className="">
-      <div className="flex flex-row mx-4">
-        <div className="flex-none">
-          <p
-            className="text-2xl font-bold"
-            style={{ fontFamily: "'Exo 2', sans-serif" }}
-          >
-            Hello Fitpeo✋,
-          </p>
-        </div>
-        <div className="flex-auto"></div>
-        <div className="bg-white rounded-2xl p-2 ml-4">
-          <div className="flex items-center rounded-sm bg-light-white ">
-            <BsSearch className="text-gray-500 text-lg block float-left cursor-pointer mr-2" />
-            <input
-              type={"search"}
-              placeholder="Search"
-              className="text-base bg-transparent w-full text-gray-600 focus:outline-none"
-            />
-          </div>
-        </div>
+      <div>
+        <Header />
       </div>
       <div>
         <Summery />
